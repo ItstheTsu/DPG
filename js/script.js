c1c69cd12f1830034c6f0d1718479a06332ddfe1
Tsu <allan.correagamex@gmail.com>
2023-10-24 10:38:10 -0300

    Base

--- js/script.js
+++ js/script.js
@@ -28,3 +28,17 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
+let imgScroll1 = document.getElementById('img-move1');
+let imgScroll2 = document.getElementById('img-move2');
+let imgScroll3 = document.getElementById('img-move3');
+let chart1 = document.getElementById('chart1');
+let chart2 = document.getElementById('chart2');
+let chart3 = document.getElementById('chart3');
+let chart4 = document.getElementById('chart4');
+
+window.addEventListener('scroll', () => {
+    let value = window.scrollY;
+
+    imgScroll1.style.left = (value * -.122)/1.2 + 'px';
+    imgScroll3.style.right = (value * -.122)/1.2 + 'px';
+})
